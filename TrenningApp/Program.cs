a31e07902596e01b3ca4fef09f46dac5436522a8
Norbert Rozmus <nrozmus@insolutions.pl>
2016-06-27 22:00:49 +0200

    test

--- TrenningApp/Program.cs
+++ TrenningApp/Program.cs
@@ -19,6 +19,7 @@ namespace TrenningApp
         {
             try
             {
+
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
 
